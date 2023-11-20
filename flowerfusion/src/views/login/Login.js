9eae04f64de8b5a8ceff069ddbc4d6835d9c479f
Admin <tranthanhhien123bt@gmail.com>
2023-11-20 09:13:42 +0700

    dev:login

---
+++ flowerfusion/src/views/login/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import LoginInput from "../../components/login/login";
+
+export class Login extends Component {
+  render() {
+    return <div> <LoginInput></LoginInput> </div>;
+  }
+}
+
+export default Login;
