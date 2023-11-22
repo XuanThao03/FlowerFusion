a5cd419f43e80ea7e006c713d52140f983581461
DESKTOP-LGBDD6D\Admin <21522611@gm.uit.edu.vn>
2023-11-22 12:05:01 +0700

    dev: bottomBanner

--- flowerfusion/src/components/banner/bottomBanner.js
+++ flowerfusion/src/components/banner/bottomBanner.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./bottomBanner.module.scss";
 import { IMG_bgBtBanner } from "../../assets/images";
+import { IC_Search } from "../../assets/icons";
 function BottomBanner() {
   return (
     <div
@@ -9,7 +10,33 @@ function BottomBanner() {
         background: `url(${IMG_bgBtBanner})`,
       }}
     >
-      BottomBanner
+      <div>
+        <p className={styles.text1}>SIGN UP TO OUR NEWSLETTER</p>
+        <p className={styles.text2}>
+          Subscribe to be the first to know about re-stocks, new releases and
+          special deals.
+        </p>
+        <form>
+          <label
+            //for="search"
+            class={styles.txtLabel}
+          >
+            Email
+          </label>
+          <div class="relative">
+            <input
+              type="search"
+              id="search"
+              class={styles.tbxContainer}
+              placeholder="Email"
+              required
+            />
+            <button type="submit" class={styles.btnSubmit}>
+              Summit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
