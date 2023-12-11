3e40da39686fd7735b157768439f250a08bb2d78
DESKTOP-LGBDD6D\Admin <vxuanthao03@gmail.com>
2023-12-11 13:29:42 +0700

    push sign up

--- flowerfusion/src/components/singup/singup.js
+++ flowerfusion/src/components/singup/singup.js
@@ -1,19 +1,35 @@
-import React, {useState} from 'react';
-import {NavLink} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {NavLink, useNavigate} from 'react-router-dom';
 import axios from 'axios';
-function Signup() {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const handleSubmit = e => {
+import {useDispatch, useSelector} from 'react-redux';
+import {register} from '../../Redux/Actions/userActions';
+import Message from '../LoadingError/Error';
+import Loading from '../LoadingError/Loading';
+const Signup = ({location, history}) => {
+  window.scrollTo(0, 0);
+  const navigate = useNavigate();
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  const redirect = window.location.search
+    ? window.location.search.split('=')[1]
+    : '/';
+
+  const userRegister = useSelector(state => state.userRegister);
+  const {error, loading, userInfo} = userRegister;
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, history, redirect]);
+
+  const submitHandler = e => {
+    console.log(firstname);
     e.preventDefault();
-    axios
-      .post(
-        'mongodb+srv://FirstUser:userpassword123@flowerfusion0.goo2ylu.mongodb.net/?retryWrites=true&w=majority',
-        {name, email, password},
-      )
-      .then(result => console.log(result))
-      .catch(err => console.log(err));
+    dispatch(register(firstname, lastname, email, password));
   };
   return (
     <div className=" align-middle flex flex-col items-center ">
@@ -23,20 +39,23 @@ function Signup() {
       <p className="text-xs font-medium font-lexend text-argent mt-3.5">
         Create your new account
       </p>
-      <form onSubmit={handleSubmit}>
+      {error && <Message variant="alert-danger">{error}</Message>}
+      {loading && <Loading />}
+      <form onSubmit={submitHandler}>
         <div className="text-xs font-[Lexend] font-light text-main-color">
           <input
             className="w-72 border-[1.4px] font-[Lexend] boder-gainsboro rounded-md h-11 p-4 mt-12 bg-transparent"
             placeholder="Firstname"
-            onChange={e => setName(e.target.value)}
+            onChange={e => setFirstname(e.target.value)}
           />
         </div>
-        {/* <div className="text-xs font-[Lexend] font-light text-main-color">
-        <input
-          className="w-72 border-[1.4px] boder-gainsboro rounded-md h-11 p-4 mt-3 bg-transparent"
-          placeholder="Lastname"
-        />
-      </div> */}
+        <div className="text-xs font-[Lexend] font-light text-main-color">
+          <input
+            className="w-72 border-[1.4px] boder-gainsboro rounded-md h-11 p-4 mt-3 bg-transparent"
+            placeholder="Lastname"
+            onChange={e => setLastname(e.target.value)}
+          />
+        </div>
 
         <div className="text-xs font-[Lexend] font-light text-main-color">
           <input
@@ -54,13 +73,20 @@ function Signup() {
           />
         </div>
 
-        <NavLink to="/myaccount" exact={true} className="mt-9">
-          <button className="bg-button-black w-72 h-10 rounded-[10px] text-white text-xs font-[Lexend] font-normal">
+        <div className="mt-10">
+          <button className="bg-button-black w-72 h-10 rounded-[10px] text-white text-xs font-[Lexend] font-normal mt-10">
             Sign up
           </button>
-        </NavLink>
+        </div>
       </form>
+
+      <NavLink
+        to={redirect ? `/login?redirect=${redirect}` : '/login'}
+        exact={true}
+        className="py-2">
+        Already have an account <strong>Login</strong>
+      </NavLink>
     </div>
   );
-}
+};
 export default Signup;
