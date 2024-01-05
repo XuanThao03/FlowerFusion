fb1667258c62716aed090de0833b661273fbbb47
DESKTOP-LGBDD6D\Admin <vxuanthao03@gmail.com>
2024-01-05 15:33:54 +0700

    fix sign up

--- server/data/users.js
+++ server/data/users.js
@@ -1,24 +1,28 @@
 import bcrypt from "bcrypt";
 const users = [
   {
-    firstname: "customer01",
-    lastname: "lastcustomer01",
+    firstname: "One",
+    lastname: "Number",
     email: "user01@gmail.com",
-    password: bcrypt.hashSync("123456", 10),
-    isAdmin: false,
+    password: bcrypt.hashSync("12345678", 10),
   },
   {
-    firstname: "customer02",
-    lastname: "lastcustomer02",
-    email: "user01@gmail.com",
-    password: bcrypt.hashSync("123456", 10),
-    isAdmin: false,
+    firstname: "Two",
+    lastname: "Number",
+    email: "user02@gmail.com",
+    password: bcrypt.hashSync("12345678", 10),
   },
   {
-    firstname: "admin01",
-    lastname: "lastadmin01",
-    email: "user01@gmail.com",
-    password: bcrypt.hashSync("123456", 10),
+    firstname: "Three",
+    lastname: "Number",
+    email: "user03@gmail.com",
+    password: bcrypt.hashSync("12345678", 10),
+  },
+  {
+    firstname: "One",
+    lastname: "Admin",
+    email: "admin01@gmail.com",
+    password: bcrypt.hashSync("12345678", 10),
     isAdmin: true,
   },
 ];
